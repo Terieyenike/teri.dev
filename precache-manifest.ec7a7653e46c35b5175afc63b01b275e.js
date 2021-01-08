8266ce2064620b4ce443498274781f7564c964b3
Teri Eyenike <teyenike1@gmail.com>
2021-01-08 07:59:05 +0100

    Updates

---
+++ precache-manifest.ec7a7653e46c35b5175afc63b01b275e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eca85dd28c26755629447d246572ffb",
+    "url": "/v1/index.html"
+  },
+  {
+    "revision": "bfc65fabc50be74353dc",
+    "url": "/v1/static/css/main.49e28efa.chunk.css"
+  },
+  {
+    "revision": "03421d58f3156c03a08c",
+    "url": "/v1/static/js/2.eb76c903.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/v1/static/js/2.eb76c903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc65fabc50be74353dc",
+    "url": "/v1/static/js/main.d16b4d6e.chunk.js"
+  },
+  {
+    "revision": "164497ae5120d179bf8b",
+    "url": "/v1/static/js/runtime-main.35a4b6e0.js"
+  },
+  {
+    "revision": "3da14e0fd16429117b1b17bf2337e0ac",
+    "url": "/v1/static/media/teri.3da14e0f.jpg"
+  }
+]);
